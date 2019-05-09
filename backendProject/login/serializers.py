6dd60f1b2e4bacf5ae4e59a92cf5d7747da38022
willyborja95 <bor_williamalexander@hotmail.com>
2019-05-09 12:53:20 -0500

    Subiendo cambios CategoryRolList

--- backendProject/login/serializers.py
+++ backendProject/login/serializers.py
@@ -6,7 +6,7 @@ class CategorySerializer (serializers.ModelSerializer):
 
     class Meta:
         model = models.Category
-        fields = ('idCategory', 'nameCategory', 'active')        
+        fields = ('idCategory', 'nameCategory', 'active')     
 
 class ItemCategorySerializer(serializers.ModelSerializer):
     
