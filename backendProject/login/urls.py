8a6091c38252647cd6e021866d145c4c0f73aaf4
willyborja95 <bor_williamalexander@hotmail.com>
2019-05-09 23:45:20 -0500

    cambios

--- backendProject/login/urls.py
+++ backendProject/login/urls.py
@@ -10,7 +10,7 @@ urlpatterns=[
     #url(r'api-token-auth/', viewsToken.obtain_auth_token, name="api-token-auth" ),    
     url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryDetail.as_view()),
     url(r'^category/$',views.CategoryList.as_view()),
-    url(r'^itemcategoryRol/$',views.ItemCategoryRolList.as_view()),
+    
     url(r'^itemcategory/(?P<pk>[0-9]+)/$', views.ItemCategoryDetail.as_view()),
     url(r'^itemcategory/$',views.ItemCategoryList.as_view()),
 
